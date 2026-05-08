ac9c15cec983e8a38f7b2104aa60cf6b17d0a28c
pulachecarrenojob <pulachecarrenojob@gmail.com>
2026-05-07 22:16:05 -0500

    add colors globals

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFF1F5AA6);
+
+  static const secondary = Color(0xFF4DA6FF);
+
+  static const background = Color(0xFFF5F7FA);
+
+  static const white = Colors.white;
+
+  static const textPrimary = Color(0xFF1A1A1A);
+
+  static const textSecondary = Color(0xFF7A7A7A);
+
+  static const card = Colors.white;
+
+  static const border = Color(0xFFE5E7EB);
+
+  static const success = Color(0xFF2EAD6B);
+
+  static const warning = Color(0xFFFFB547);
+}
