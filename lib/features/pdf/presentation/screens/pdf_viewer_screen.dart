381b0b36406b6cc3a8f1b2d2ebd105cd35972eef
pulachecarrenojob <pulachecarrenojob@gmail.com>
2026-05-17 22:32:50 -0500

    I have error because execute openPDF inside the build()

--- lib/features/pdf/presentation/screens/pdf_viewer_screen.dart
+++ lib/features/pdf/presentation/screens/pdf_viewer_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 import '../../../../core/storage/history_service.dart';
 
-class PdfViewerScreen extends StatelessWidget {
+class PdfViewerScreen extends StatefulWidget {
   final String pdfUrl;
   final String title;
 
   const PdfViewerScreen({super.key, required this.pdfUrl, required this.title});
 
+  @override
+  State<PdfViewerScreen> createState() => _PdfViewerScreenState();
+}
+
+class _PdfViewerScreenState extends State<PdfViewerScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    HistoryService.addHistory(widget.title, 'Documento Clínico');
+
+    openPdf();
+  }
+
   Future<void> openPdf() async {
-    final Uri url = Uri.parse(pdfUrl);
+    final Uri url = Uri.parse(widget.pdfUrl);
 
     if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
       throw Exception('No se pudo abrir el PDF');
@@ -18,12 +33,8 @@ class PdfViewerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    HistoryService.addHistory(title, 'Documento Clínico');
-
-    openPdf();
-
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(widget.title)),
 
       body: const Center(child: CircularProgressIndicator()),
     );
