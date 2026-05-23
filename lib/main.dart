6d2049813ac008f1daa0c159c308469694d5ce7c
pulachecarrenojob <pulachecarrenojob@gmail.com>
2026-05-23 16:11:06 -0500

    dowland firestore

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:odontologia_app/shared/widgets/main_navigation.dart';
+
 import 'firebase_options.dart';
+
 import 'core/theme/app_theme.dart';
 import 'core/providers/theme_provider.dart';
+
 import 'features/auth/presentation/screens/login_screen.dart';
 import 'features/auth/presentation/screens/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await Hive.initFlutter();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -32,7 +37,7 @@ class MyApp extends ConsumerWidget {
 
       darkTheme: ThemeData.dark(),
 
-      home: const MainNavigation(),
+      home: const AuthGate(),
     );
   }
 }
